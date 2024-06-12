96a0e4bbc4b168fcb66bac3bd71915c86c2aa684
hmquann <mquan171@gmail.com>
2024-06-12 18:30:09 +0700

    push project

---
+++ src/main/java/org/example/motorbikerental/MotorbikeRentalApplication.java
@@ -0,0 +1,13 @@
+package org.example.motorbikerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorbikeRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorbikeRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/motorbikerental/MotorbikeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.motorbikerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorbikeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
